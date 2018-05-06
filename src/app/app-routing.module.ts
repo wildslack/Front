19202088a6a978966a5c6bd289e1ba1a0bb05f7b
Unknown <boujenah.bruno@gmail.com>
2018-05-06 23:01:27 +0200

    Creation Main page ( en cours )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
