27a7f94a8a30319c913fa76ede727f875c3940d0
Unknown <boujenah.bruno@gmail.com>
2018-05-07 12:54:02 +0200

    Main Page ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterModule } from './register/register.module';
+import { WelcomeComponent } from './main-page/welcome/welcome.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
-  {path:'register',component:RegisterComponent}
+  {path:'register',component:RegisterComponent},
+  {path:'welcome',component:WelcomeComponent},
+  {path:'main-page',component:MainPageComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
+import { MainPageModule } from './main-page/main-page.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderModule } from './header/header.module';
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
+    MainPageModule
    
 
   ],
