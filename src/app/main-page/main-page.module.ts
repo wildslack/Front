94d3abe870fb688ed5b8bfd026dbdac66738f6ac
Unknown <boujenah.bruno@gmail.com>
2018-06-04 16:27:23 +0200

    components and main-Page

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -4,15 +4,17 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MainPageComponent } from './main-page.component';
 import { RouterModule } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidePannelComponent } from './side-pannel/side-pannel.component';
+import { SeizureComponent } from './seizure/seizure.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
     RouterModule
 
   ],
-  declarations: [WelcomeComponent, MainPageComponent, ChatComponent],
-  exports: [WelcomeComponent, MainPageComponent, ChatComponent],
+  declarations: [WelcomeComponent, MainPageComponent, ChatComponent, NavbarComponent, SidePannelComponent, SeizureComponent],
+  exports: [WelcomeComponent, MainPageComponent, ChatComponent, NavbarComponent, SidePannelComponent, SeizureComponent],
 })
 export class MainPageModule { }
---
+++ src/app/main-page/seizure/seizure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seizure',
+  templateUrl: './seizure.component.html',
+  styleUrls: ['./seizure.component.scss']
+})
+export class SeizureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
